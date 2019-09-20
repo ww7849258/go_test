a9503d8717fe1926d577bef91824910edbaa65a6
horus.wu <horus.wu@bindo.com>
2019-09-20 12:29:45 +0800

    hello

---
+++ hello/hello.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func main() {
+	fmt.Println("Hello GO")
+}
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return getPrefix(language) + name
+}
+
+func getPrefix(language string) (prefix string) {
+	//var prefix string
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
