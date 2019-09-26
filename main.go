52262decc93867ea955291d36dfeba53c0198be1
horus.wu <horus.wu@bindo.com>
2019-09-26 18:55:46 +0800

    struct and interface

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("hello Horus")
+	now := time.Now().Format("2006-01-02 15:04:05")
+	fmt.Println(now[:len("2006-01-02")])
+	fmt.Println(now[len("2006-01-02 "):])
 }
